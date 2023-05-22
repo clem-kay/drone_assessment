2509b40f83d9c86da5af24b447b7c71e272813a3
clem-kay <ckoomson75@gmail.com>
2023-05-22 21:37:36 +0000

    added the models

---
+++ drone/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+# Create your models here.
+class Drone(models.Model):
+    DRONE_MODEL = [
+        ('LIGHTWEIGHT', 'LIGHTWEIGHT'),
+        ('MIDDLEWEIGHT','MIDDLEWEIGHT'),
+        ('CRUISERWEIGHT', 'CRUISERWEIGHT'),
+        ('HEAVYWEIGHT','HEAVYWEIGHT')]
+    DRONE_STATE = [
+         ('IDLE','IDLE'),
+        ('LOADING', 'LOADING'),
+         ('LOADED', 'LOADED'),
+        ('DELIVERING', 'DELIVERING'),
+        ('DELIVERED', 'DELIVERED'),
+        ('RETURNING','RETURNING'),]
+    serial_number = models.CharField(max_length=100)
+    drone_model = models.CharField(
+        max_length=10,
+        choices=DRONE_MODEL,
+        )
+    weight_limit = models.CharField()
+    batter_capacity = models.CharField()
+    state = models.CharField(
+        max_length=10,
+        choices=DRONE_STATE,
+        default='IDLE',
+        )
+
+    def __str__(self):
+        return f"{self.drone_model} {self.serial_number}"
---
+++ medication/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Medication(models.Model):
+    name = models.TextField(max_length=200)
+    weight = models.CharField()
+    code = models.CharField()
+    image = models.ImageField(upload_to='item_images',blank=True,null=True)
+
+    def __str__(self):
+        return f'{self.name}'
